f31473df7ddc4b3697eeb7dc479489de0d929e8d
Ashray <ashraypatil0415@gmail.com>
2023-05-22 03:03:14 -0700

    comments added to posts

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,25 +1,34 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    order: [['created_at', 'DESC']],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
+     order: [['created_at', 'DESC']],
+ attributes: [
+   'id',
+   'post_url',
+   'title',
+   'created_at',
+   [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+ ],
+ include: [
+   // include the Comment model here:
+   {
+     model: Comment,
+     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+     include: {
+       model: User,
+       attributes: ['username']
+     }
+   },
+   {
+     model: User,
+     attributes: ['username']
+   }
+ ]
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -30,22 +39,29 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
+   order: [['created_at', 'DESC']],
+ attributes: [
+   'id',
+   'post_url',
+   'title',
+   'created_at',
+   [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+ ],
+ include: [
+   // include the Comment model here:
+   {
+     model: Comment,
+     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+     include: {
+       model: User,
+       attributes: ['username']
+     }
+   },
+   {
+     model: User,
+     attributes: ['username']
+   }
+ ]
   })
     .then(dbPostData => {
       if (!dbPostData) {
